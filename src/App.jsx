0d73c7cd2f3a63a1cadce9e8b5c8dadc38518b7a
Dimitar <wopz1337@gmail.com>
2022-11-18 17:28:46 +0200

    Added Router & Import All Components In App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,44 @@
 import "./App.css";
-
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import {
+  Exchanges,
+  Homepage,
+  News,
+  Cryptocurrencies,
+  CryptoDetails,
+  Navbar,
+} from "./components";
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <Router>
+      <div className="app">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="main">
+          <Layout>
+            <div className="routes">
+              <Routes>
+                <Route path="/" element={<Homepage />} />
+
+                <Route path="/exchanges" element={<Exchanges />} />
+
+                <Route
+                  path="/cryptocurrencies"
+                  element={<Cryptocurrencies />}
+                />
+
+                <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+
+                <Route path="/news" element={<News />} />
+              </Routes>
+            </div>
+          </Layout>
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
