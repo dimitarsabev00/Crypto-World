56c63a67e2a9ce8aff9bcb927025936a6892a03b
Dimitar <wopz1337@gmail.com>
2022-11-18 23:29:56 +0200

    Added Redux Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "antd/dist/antd.css";
+import { Provider } from "react-redux";
+import store from "./app/store";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
