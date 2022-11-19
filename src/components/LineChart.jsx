c5987c7f93e50e497e729f29f93e2da4a0bf55f5
Dimitar <wopz1337@gmail.com>
2022-11-19 23:06:56 +0200

    Fix Issue With Chart

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
 import { Line } from "react-chartjs-2";
 import { Col, Row, Typography } from "antd";
 
-const { Title } = Typography;
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
-
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory?.data?.history[i].price);
   }
@@ -17,6 +34,10 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
       new Date(coinHistory?.data?.history[i].timestamp).toLocaleDateString()
     );
   }
+  const options = {
+    responsive: true,
+  };
+
   const data = {
     labels: coinTimestamp,
     datasets: [
@@ -30,31 +51,20 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
     ],
   };
 
-  const options = {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        },
-      ],
-    },
-  };
-
+  console.log(coinPrice);
   return (
     <>
       <Row className="chart-header">
-        <Title level={2} className="chart-title">
+        <Typography.Title level={2} className="chart-Typography.title">
           {coinName} Price Chart{" "}
-        </Title>
+        </Typography.Title>
         <Col className="price-container">
-          <Title level={5} className="price-change">
+          <Typography.Title level={5} className="price-change">
             Change: {coinHistory?.data?.change}%
-          </Title>
-          <Title level={5} className="current-price">
+          </Typography.Title>
+          <Typography.Title level={5} className="current-price">
             Current {coinName} Price: $ {currentPrice}
-          </Title>
+          </Typography.Title>
         </Col>
       </Row>
       <Line data={data} options={options} />
