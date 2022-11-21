07c5e6b8831c7b01b9d2e75126d31c3b4a31e2cd
Dimitar <wopz1337@gmail.com>
2022-11-21 18:40:52 +0200

    Added Query getAllCryptos

--- src/services/coinGeckoAPI.js
+++ src/services/coinGeckoAPI.js
@@ -10,7 +10,9 @@ export const coinGeckoAPI = createApi({
       query: () => createRequest(`/global`),
     }),
     getAllCryptos: builder.query({
-      query: () => createRequest(`/coins/list`),
+      query: () =>
+        createRequest(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&sparkline=false
+      `),
     }),
   }),
 });
