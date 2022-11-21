8d7e7f7a4d473435652b3954e75a0b0ecfcd612c
Dimitar <wopz1337@gmail.com>
2022-11-21 14:16:47 +0200

    Fix Issue Name In coinGeckoAPI.js File

--- src/services/coinGeckoAPI.js
+++ src/services/coinGeckoAPI.js
@@ -2,14 +2,18 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseUrl = "https://api.coingecko.com/api/v3";
 const createRequest = (url) => ({ url });
-export const cryptoApi = createApi({
-  reducerPath: "cryptoApi",
+export const coinGeckoAPI = createApi({
+  reducerPath: "coinGeckoAPI",
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptosGlobalData: builder.query({
       query: () => createRequest(`/global`),
     }),
+    getAllCryptos: builder.query({
+      query: () => createRequest(`/coins/list`),
+    }),
   }),
 });
 
-export const { useGetCryptosGlobalDataQuery } = coinGeckoAPI;
+export const { useGetCryptosGlobalDataQuery, useGetAllCryptosQuery } =
+  coinGeckoAPI;
