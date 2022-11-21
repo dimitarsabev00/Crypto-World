b264e26a7b6ac908d40b427f4c060dd7bbbff502
Dimitar <wopz1337@gmail.com>
2022-11-21 14:13:30 +0200

    Added coinGeckoAPI In Redux Store

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import { cryptoNewsApi } from "../services/cryptoNewsApi";
+import { coinGeckoAPI } from "../services/coinGeckoAPI";
 
 export default configureStore({
   reducer: {
     [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+    [coinGeckoAPI.reducerPath]: coinGeckoAPI.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(cryptoNewsApi.middleware),
+    getDefaultMiddleware().concat(
+      cryptoNewsApi.middleware,
+      coinGeckoAPI.middleware
+    ),
 });
