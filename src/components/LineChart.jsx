ecdeeddef723a544bb5f2ee4c1e0db3a43e0639c
Dimitar <wopz1337@gmail.com>
2022-11-21 20:07:24 +0200

    Fix Issue With LineChart Component

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -32,8 +32,8 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   };
 
   const data = {
-    labels: coinHistory?.data.history.map((coin) => {
-      let date = new Date(coin["timestamp"]);
+    labels: coinHistory?.prices.map((coin) => {
+      let date = new Date(coin[0]);
       let time =
         date.getHours() > 12
           ? `${date.getHours() - 12}:${date.getMinutes()} PM`
@@ -43,13 +43,14 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
     datasets: [
       {
         label: "Price In USD",
-        data: coinHistory?.data.history.map((coin) => coin["price"]),
+        data: coinHistory?.prices.map((coin) => coin[1]),
         fill: false,
         backgroundColor: "#0071bd",
         borderColor: "#0071bd",
       },
     ],
   };
+  console.log(coinHistory?.prices);
   return (
     <>
       <Row className="chart-header">
@@ -57,9 +58,6 @@ const LineChart = ({ coinHistory, currentPrice, coinName }) => {
           {coinName} Price Chart{" "}
         </Typography.Title>
         <Col className="price-container">
-          <Typography.Title level={5} className="price-change">
-            Change: {coinHistory?.data?.change}%
-          </Typography.Title>
           <Typography.Title level={5} className="current-price">
             Current {coinName} Price: $ {currentPrice}
           </Typography.Title>
