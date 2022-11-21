464256c3bb19cb7bac6bf194726a4ac6d1da9cb5
Dimitar <wopz1337@gmail.com>
2022-11-21 14:13:55 +0200

    Render Global Stats In HomePage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,8 +3,14 @@ import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
 import { Link } from "react-router-dom";
 import { Cryptocurrencies, News } from "../components";
+import { useGetCryptosGlobalDataQuery } from "../services/coinGeckoAPI";
+import Loader from "./Loader";
 const { Title } = Typography;
 const Homepage = () => {
+  const { data, isFetching } = useGetCryptosGlobalDataQuery();
+  const globalStats = data?.data;
+  if (isFetching) return <Loader />;
+
   return (
     <>
       <Title level={2} className="heading">
@@ -12,18 +18,28 @@ const Homepage = () => {
       </Title>
       <Row gutter={[32, 32]}>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies" value="5" />
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={globalStats?.active_cryptocurrencies}
+          />
         </Col>
-
         <Col span={12}>
-          <Statistic title="Total Market Cap:" value="5" />
+          <Statistic title="Total Exchanges" value={globalStats?.markets} />
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume" value="5" />
+          <Statistic
+            title="Total Market Cap Change 24h:"
+            value={`${globalStats?.market_cap_change_percentage_24h_usd.toFixed(
+              2
+            )}%`}
+          />
         </Col>
-
         <Col span={12}>
-          <Statistic title="Total Markets" value="5" />
+          <Statistic
+            title="Dominance"
+            value={`BTC: ${globalStats?.market_cap_percentage?.btc.toFixed(2)}%
+            • ETH: ${globalStats?.market_cap_percentage?.eth.toFixed(2)}% `}
+          />
         </Col>
       </Row>
       <div className="home-heading-container">
